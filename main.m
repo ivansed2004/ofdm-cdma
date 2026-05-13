7fd3ca9aec51314ba24814fe9df5da195865de29
ivan <id2like0to0know4@gmail.com>
2026-05-13 18:24:46 +0300

    255-length PN sequences are not considered anymore

--- main.m
+++ main.m
@@ -5,7 +5,7 @@ clear all;
 graphics_toolkit("fltk");
 
 % 1. Считываем все данные из CSV в одну общую матрицу
-strings = dlmread('biterrors.csv', ';');
+strings = dlmread('biterrors.csv');
 
 % 2. Извлекаем первую строку в массив pSN
 pSN = strings(1, :);
@@ -19,10 +19,10 @@ set(gca, 'ColorOrder', jet(num_curves));
 hold on;
 
 fig1 = figure();
-plot( pSN, BitErrors, 'linewidth', 2 );
+plot( pSN, BitErrors, 'linewidth', 3 );
 xlabel('SNR (dB)'); ylabel('BitError'); title('Битовые ошибки, BER');
-legend({'OFDM+QPSK', 'OFDM+CDMA(15)', 'OFDM+CDMA(31)', 'OFDM+CDMA(63)', 'OFDM+CDMA(127)', 'OFDM+CDMA(255)'},
-       'location', 'northeast',
+legend({'OFDM+QPSK', 'OFDM+CDMA(15)', 'OFDM+CDMA(31)', 'OFDM+CDMA(63)', 'OFDM+CDMA(127)'},
+       'location', 'southwest',
        'fontsize', 25);
 grid on;
 set(gca, 'FontSize', 25, 'yscale', 'log');
