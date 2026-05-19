de0d4241fa9832b7cd3b284a444750c01acb90ee
ivan <id2like0to0know4@gmail.com>
2026-05-20 01:08:17 +0300

    1. normalization deleted; 2. Comments added

--- acf.m
+++ acf.m
@@ -1,3 +1,4 @@
+% Вычисление АКФ для заданной последовательности s
 function [R, lag] = acf( s )
     ls = length(s);
     lag = -ls:1:ls;
@@ -7,17 +8,11 @@ function [R, lag] = acf( s )
         l = lag(j);
         for i = 0:ls-1
             if ( (i-l+1) <= 0 || (i-l+1) > ls )
-                Rs = Rs + xor( s(i+1), 0 );
+                Rs = Rs + 0;
             else
-                Rs = Rs + xor( s(i+1), s(i-l+1) );
+                Rs = Rs + s(i+1)*s(i-l+1);
             endif
         endfor
         R(j) = Rs;
-    endfor
-    
-    maxR = length(s);
-    
-    for k = 1:length(R)
-        R(k) = (maxR - R(k)) / maxR;
     endfor
 endfunction
