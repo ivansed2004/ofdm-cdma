8d273198113f1d0b45701ef4859be09bed36ace2
ivan <id2like0to0know4@gmail.com>
2026-05-20 01:29:02 +0300

    1. 15-length Gold code is not used; 2. Commends added; 3. pdf generation added

--- main.m
+++ main.m
@@ -1,19 +1,20 @@
+% Скрипт для генерации графиков BER(SNR) по ранее сгенерированным файлам biterrors.csv
 clc;
 close all;
 clear all;
 
 graphics_toolkit("fltk");
 
-% 1. Считываем все данные из CSV в одну общую матрицу
+% Считываем все данные из CSV в одну общую матрицу
 strings = dlmread('biterrors.csv');
 
-% 2. Извлекаем первую строку в массив pSN
+% Извлекаем первую строку в массив pSN
 pSN = strings(1, :);
 
-% 3. Извлекаем остальные строки
+% Извлекаем остальные строки
 BitErrors = strings(2:end, :);
-
 num_curves = size(BitErrors, 1);
+
 % Устанавливаем порядок цветов для этих осей
 set(gca, 'ColorOrder', jet(num_curves));
 hold on;
@@ -21,8 +22,10 @@ hold on;
 fig1 = figure();
 plot( pSN, BitErrors, 'linewidth', 3 );
 xlabel('SNR (dB)'); ylabel('BitError'); title('Битовые ошибки, BER');
-legend({'OFDM+QPSK', 'OFDM+CDMA(15)', 'OFDM+CDMA(31)', 'OFDM+CDMA(63)', 'OFDM+CDMA(127)'},
-       'location', 'southwest',
-       'fontsize', 25);
+legend({'QPSK+OFDM', 'CDMA(31)+OFDM', 'CDMA(63)+OFDM', 'CDMA(127)+OFDM'},
+       'location', 'northeast',
+       'fontsize', 5);
 grid on;
-set(gca, 'FontSize', 25, 'yscale', 'log');
+set(gca, 'FontSize', 20, 'yscale', 'log');
+
+print("biterrors.pdf", "-S768,1024", "-dpdf");
