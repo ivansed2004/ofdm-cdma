227724ce48580d09308cf34728eb652c58c8f1cf
ivan <id2like0to0know4@gmail.com>
2026-05-20 01:02:26 +0300

    Comments added

--- CDMA-OFDM.m
+++ CDMA-OFDM.m
@@ -1,3 +1,4 @@
+% ======================== МОДЕЛЬ CDMA+OFDM =======================
 pkg load communications;
 pkg load signal;
 clc;
@@ -9,59 +10,69 @@ graphics_toolkit("fltk");
 addpath('gold');
 addpath('hadamard');
 
+% ======================== Параметры модели =======================
+% 1. Общее количество бит
 nBit = 20000;
-% Ввод параметров модуляции
-M=4; % число символов в канале ПД
-k=log2(M); % число бит на один символ
-nSym = ceil(nBit / k); %число символов данных
-%
-FM=64;%задать порядок OFDM символа и кол-во поднесущих=(FM/4 -1)
-kF=log2(FM);
+% 2. Количество уникальных символов данных
+M = 4;
+% 3. Количество бит на символ
+k = log2(M);
+nSym = ceil(nBit / k);
+% 4. Индекс OFDM и кол-во поднесущих=(FM/4 -1)
+FM = 64;
+kF = log2(FM);
+% 5. Количество каналов (= количеству потоков)
+channels = 4;
+% 6. Полином, порождающий ПСП
+% старшая степень полинома
+pd = 6;
+% степени полинома с единичным коэффициентом
+polynom = [pd, 1];
+% 7. Коэффициент децимации
+dec = 5;
+% 8. Коэффициент расширения спектра
+sf = 2^polynom(1) - 1;
 
-% формирование исходной битовой последовательности данных
+% ================= Подготовка данных для передачи ================
+% Формирование исходной битовой последовательности данных
 bit_data = randi([0, 1], 1, nBit); bit_data = [bit_data, zeros(1, nSym*k-nBit)];
 bit_matrix = reshape(bit_data, k, nSym);
-% формирование исходной символьной последовательности данных
+% Формирование исходной символьной последовательности данных
 weights = 2.^(k-1:-1:0);
 data = weights * bit_matrix;
 
-%% формирование базиса быстрого преобразования Уолша
-channels = 4;
+%% Формирование базиса быстрого преобразования Уолша
 walsh_basis = hadamard_code(channels);
 
-% спецификация кодов Голда
-##spec = {
-##  {4, [1], 7};
-##  {5, [2], 9};
-##  {6, [1], 11}
-##  {7, [1], 13}
-##  {8, [4, 3, 1], 33}
-##};
+% Отношение сигнал/шум (dB)
+pSN = -25:1:-9;
 
-pSN = -25:1:0; # SNR (dB)
+%% Матрица кодов Голда, задаваемая в соответствии со след. параметрами:
+%% 1. Степенью полинома, формирующего регистр сдвига
+%% 2. Коэффициентами при степенях полинома, начиная со старшей степени
+%% 3. Коэффициентом децимации
+G = gold_code( pd, polynom, dec );
 
-% подсчет ошибок
+% Подсчет ошибок
 pSNsize = length(pSN);
 SymErrors = zeros(1, pSNsize);
 BitErrors = zeros(1, pSNsize);
 
-%% Generation Matrix (G)
-G = gold_code(8, [4, 3, 1], 33);
-
-sf = size(G)(2); % коэффициент расширения (DSSS) / число чипов на символ
-cbr = sf / k; % chip to bit ratio (сколько в среднем чипов приходится на бит в пределах одного символа)
-nChip = nSym*sf; % общее число чипов, передаваемых в СПД
-
-%-----------------Transmitter (CDMA-OFDM)--------------------
-spreading_code = reshape( G(1:M*channels, :), [channels, M, sf] ); % расширяющие коды
+% ===================== Передатчик (CDMA+OFDM) ====================
+% Расширяющие коды Голда, сгруппированные по каналам
+spreading_code = reshape( G(1:M*channels, :), [channels, M, sf] );
+% Замена 0 на 1, 1 на -1
 spreading_code = 1 - 2*spreading_code;
 
+% Количество итераций, необходимых для передачи символов по всем каналам
 dd = ceil(length(data) / channels);
-ch_data = [ data, zeros(1, channels*dd-length(data)) ]; % заполнение нулями для в случае нехватки символов данных
-ch_data = reshape( ch_data, dd, channels ).'; % исходные данные, сгруппированные по каналам
-spr_ch_data = zeros(channels, dd, sf); % те же данные, представленные в соответствующих кодах
-
-% алгоритм заполнения тензора spr_ch_data
+% Заполнение нулями в случае нехватки символов данных
+ch_data = [ data, zeros(1, channels*dd-length(data)) ];
+% Исходные данные, сгруппированные по каналам
+ch_data = reshape( ch_data, dd, channels ).';
+% Те же данные, представленные в соответствующих кодах
+spr_ch_data = zeros(channels, dd, sf);
+% Алгоритм заполнения тензора spr_ch_data
 for i = 1:1:channels
     for j = 1:1:dd
         sym = ch_data(i, j);
@@ -69,20 +80,20 @@ for i = 1:1:channels
     end
 end
 
-ifwt_output = zeros(channels, sf, dd); % тензор с коэффициетами ОБПУ
+% Тензор с коэффициетами ОБПУ
+ifwt_output = zeros(channels, sf, dd);
 for i = 1:1:dd
     sl = squeeze(spr_ch_data(:, i, :));
     ifwt_output(:, :, i) = (walsh_basis * sl) / channels;
 end
 
-% набор вещественных данных
+% Набор вещественных данных
 real_d = ifwt_output(:).';
-% набор комплексных данных (преобразование Гильберта)
+% Набор комплексных данных (преобразование Гильберта)
 d = hilbert(real_d);
 
-% ================================ %
-
-%Готовим матрицу data к OFDM, Используем технологию ACO-OFDM;
+% -------------------- Формирование OFDM-кадра --------------------
+% Готовим матрицу data к OFDM. Используем технологию ACO-OFDM;
 iDF=FM/4-1; datOFDM=[]; % iDF - число поднесущих
 Jdat = 0; bdf = [];
 padding = 0;
@@ -104,28 +115,27 @@ for jfl=1:Jdat;
   endfor
   datOFDM=[datOFDM ZagM];
 end
-%
 Sof=ifft(datOFDM,FM);
 
 %формируем и приклеиваем CyclicPrefix (CP)
 Sd0f=(-4).*ones(1,Jdat); S1of=[Sd0f;Sof;Sd0f];
-%
 s=reshape(Sof,1,FM*Jdat); psmim=abs(max(s));% без СР
 snss=floor(20*log10(rms(s)));
 s11=s.+psmim+2;% фрейм без СР
 s1z=reshape(S1of,1,(FM+2)*Jdat); S11z=s1z.+psmim+0.5;% фрейм плюс СР
+% =================================================================
 
 for p = 1:1:pSNsize
-  %-------------- Channel ----------------
+  % ========================= Канал AWGN ==========================
   z=awgn(s11,pSN(p),snss);% помеха
   ZkanWithawgn=real(z)*cos(2*pi)+imag(z)*sin(2*pi);
   ZkanWithoutAwgn=real(s11)*cos(2*pi)+imag(s11)*sin(2*pi);
   Zs1kan=real(S11z)*cos(2*pi)+imag(S11z)*sin(2*pi);% ОЧРК с СР
   Zzs1kan=awgn(Zs1kan,pSN(p),snss-6);% помеха к ОЧРК с СР
-  %-------------- Receiver ----------------
+
+  % ===================== Приемник (CDMA+OFDM) ====================
   yZkan=buffer(Zzs1kan,(FM+2));
   ySkan=yZkan(2:(FM+1),:);
-  %yCkan
   yRd=fft(ySkan,FM);
   yiRDF=size(yRd);
   yRskF=yRd(2:2:FM,:);
@@ -134,16 +144,17 @@ for p = 1:1:pSNsize
   ysr=ybdf1;
   dyCap = real(ysr); dyCap = dyCap(1:length(dyCap)-padding);
 
-  % коэффициенты БПУ + белый гауссов шум
+  % Коэффициенты БПУ с белым шумом
   ifwt_output_awgn = reshape( dyCap, [channels, sf, dd] );
-  % обратное БПУ с зашумленными коэффициентами (значения чипов + белый гауссов шум)
+  % Обратное БПУ с зашумленными коэффициентами (значения чипов + белый шум)
   fwt_output_awgn = zeros(channels, sf, dd);
   for i = 1:1:dd
       sl = squeeze(ifwt_output_awgn(:, :, i));
+      % Прямое БПУ для восстановления исходных чипов
       fwt_output_awgn(:, :, i) = (walsh_basis * sl);
   endfor
 
-  % расчет ошибок (оценка наибольшего правдоподобия)
+  % Расчет ошибок (оценка наибольшего правдоподобия)
   symE = 0;
   bitE = 0;
   for i = 1:1:channels
@@ -152,6 +163,7 @@ for p = 1:1:pSNsize
           true_sym = 0;
           R = 0;
           for m = 1:1:M
+              % Нахождение корреляции между принятой и опорной последовательностью
               Rs = rec_corr( squeeze(spreading_code(i, m, :)), squeeze(fwt_output_awgn(i, :, j)) );
               if (Rs > R)
                   R = Rs;
@@ -169,7 +181,9 @@ for p = 1:1:pSNsize
   SymErrors(p) = symE;
   BitErrors(p) = bitE;
 endfor
+% =================================================================
 
+% ============ Сохранение данных и построение графиков ============
 dlmwrite('symerrors.csv', SymErrors(1, :)./nSym, '-append', 'delimiter', ';');
 dlmwrite('biterrors.csv', BitErrors(1, :)./nBit, '-append', 'delimiter', ';');
 
